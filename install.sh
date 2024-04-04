361b1e76cd2ca6dafd17aee52c91f72e539e3d01
Matthew Yang <141765903+matthewyang204@users.noreply.github.com>
2024-04-04 09:38:10 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "Downloading..."
 cd ~/Downloads
-curl -LO https://github.com/matthewyang204/Chase-Game/releases/download/1.0/Install.Chase.Game.2.0.dmg
+curl -LO https://github.com/matthewyang204/Chase-Game/releases/download/2.0/Install.Chase.Game.2.0.dmg
 echo "Installing with sudo, enter password if asked..."
-hdiutil attach ~/Downloads/Install.Chase.Game.1.0.dmg
+hdiutil attach ~/Downloads/Install.Chase.Game.2.0.dmg
 sudo cp -R /Volumes/Chase\ Game/Chase\ Game.app /Applications/Chase\ Game.app
 echo "Installation complete, cleaning up..."
 hdiutil detach /Volumes/Chase\ Game
-sudo rm -R ~/Downloads/Install.Chase.Game.1.0.dmg
+sudo rm -R ~/Downloads/Install.Chase.Game.2.0.dmg
 echo "done"
